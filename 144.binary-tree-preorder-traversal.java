b31dabe614c1144adb9bf4d75d48e73ce086e786
tianqiyang <bruceyoungsysu@gmail.com>
2020-07-18 13:51:06 -0400

    graph day 5

---
+++ 144.binary-tree-preorder-traversal.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=144 lang=java
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if(root==null) return res;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            if(node == null) continue;
+            res.add(node.val);
+            stack.push(node.right);
+            stack.push(node.left);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
